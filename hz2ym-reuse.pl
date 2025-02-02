1e138d7acd49cdac7e06b2db6eb91db2c08aabd7
Jonas S Karlsson <jsk@yesco.org>
2025-02-02 20:06:57 +0700

    cleanup

--- hz2ym-reuse.pl
+++ hz2ym-reuse.pl
@@ -4,50 +4,37 @@ while(<>) {
     $abc= ""; $vol= "";
     $n= 0;
     while (/(\d+)v\s*(\d+)Hz/g) {
+        @lastf= @newf if $n==0;
+
         @newf= (0,0,0);
         my ($v, $f)= ($1, $2);
         # TODO: because skip, need to prolong last commmand if no tone generated at this beat?
         next if $f<100;
 
-        if ($f==$lastf[0]) {
-          $newf[0]= $f;
-        } elsif ($f==$lastf[1]) {
-          $newf[1]= $f;
-        } elsif ($f==$lastf[1]) {
-          $newf[2]= $f;
+        if ($f==$lastf[0]) {          $newf[0]= $f;
+        } elsif ($f==$lastf[1]) {     $newf[1]= $f;
+        } elsif ($f==$lastf[1]) {     $newf[2]= $f;
         } else {
           my $x= 0;
           while($x<3) {
-            if (!$newf[$x]) { $newf[$x]= $f; last; }
+            if (!$newf[$x]) {         $newf[$x]= $f; last; }
             $x++;
           }
           if ($x>=3) { print "can't allocate counter!\n$_\n"; exit(1); }
         }
 
-        $vol=$abc="" if !$n;
-        $p= 1000000/16/$f;
-        $abc.= sprintf("\\x%02x\\x%02x", $p&255, $p/256);
-
         # static ymu16 ymVolumeTable[16] =
         # {62,161,265,377,580,774,1155,1575,2260,3088,4570,6233,9330,13187,21220,32767};
         # $v seems we have values 0...127?
-        $v= int($v/16); # lol
-        $v= 15 if $v>15;
-        # TODO: lol
-        $v= 15;
 
-        $newf[$n]= $f;
-        $newv[$n]= $v;
-
-        $vol.= sprintf("\\x%02x", $v);
         $n++;
         last if $n==3;
     }
     if ($n) {
         $abc= "";
-        $p= !$newf[0]? 0xffff: 1000000/16/$newf[0]; $abc.= sprintf("\\x%02x\\x%02x", $p&255, $p/256);
-        $p= !$newf[1]? 0xffff: 1000000/16/$newf[1]; $abc.= sprintf("\\x%02x\\x%02x", $p&255, $p/256);
-        $p= !$newf[2]? 0xffff: 1000000/16/$newf[2]; $abc.= sprintf("\\x%02x\\x%02x", $p&255, $p/256);
+        $p= !$newf[0]? 0: 1000000/16/$newf[0]; $abc.= sprintf("\\x%02x\\x%02x", $p&255, $p/256);
+        $p= !$newf[1]? 0: 1000000/16/$newf[1]; $abc.= sprintf("\\x%02x\\x%02x", $p&255, $p/256);
+        $p= !$newf[2]? 0: 1000000/16/$newf[2]; $abc.= sprintf("\\x%02x\\x%02x", $p&255, $p/256);
 
         # TODO: vol
         $vol= "";
