28fa37ecaecd9f691aee94e07b2482b4771bf4e0
Jonas S Karlsson <jsk@yesco.org>
2026-04-23 16:12:16 +0700

    trying to get rid of 50hz

--- hz2sox.pl
+++ hz2sox.pl
@@ -4,11 +4,20 @@ while(<>) {
         my ($v, $f)= ($1, $2);
         # TODO: because skip, need to prolong last commmand if no tone generated at this beat?
         next if $f<100;
-        print "synth " if !$n;
+#        print "synth 0.2" if !$n; # clear tones but too slow
+        print "synth 0.02" if !$n;
         #print "\t0.032 sin $f";
-        print "\t0.02 sin $f";
+	#        print "\t0.02 sin $f";
+	print " sin $f";
+	# continue at phase - nodiff
+#	print " sin +$f";
         $n++;
         last if $n==3;
     }
-    print "\n" if $n;
+
+    if ($n) {
+	# doesn't help really - muddles
+	print " fade q 0.001 0.02 0.0003";
+	print "\n";
+    }
 }
